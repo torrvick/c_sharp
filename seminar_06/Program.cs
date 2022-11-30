e41c6632790a7593d9b3c9515a60221501b1e17d
Виктор Петкевич <torrvick@gmail.com>
2022-11-30 16:32:54 +0300

    Some changes for Seminar_06

--- seminar_06/Program.cs
+++ seminar_06/Program.cs
@@ -2,9 +2,72 @@
 // Пользователь вводит с клавиатуры M чисел. 
 // Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
 
+/* Console.WriteLine($"Введите числа, каждое с новой строки. Для окончания ввода нажмите Enter без ввода числа.");
+int[] array = GetArray();
+Console.WriteLine($"Введенный массив:\n[{string.Join(", ", array)}]");
+Console.WriteLine($"Введено {PosCount(array)} {NumCompletion(PosCount(array))} больше ноля");
+
+int PosCount(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+        if (el > 0) count++;
+
+    return count;
+}
+
+string NumCompletion(int number)
+{
+    switch (number)
+    {
+        case 1:
+            return "число";
+        case 2:
+        case 3:
+        case 4:
+            return "числа";
+        default:
+            return "чисел";
+    }
+}
+ */
+
+//----------------------------------------------------------------
+// Задача 2 (43)
+// Написать программу, которая на вход принимает массив из любого количества элементов 
+// (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, 
+// но отсортированный по возрастанию(от меньшего числа к большему).
+
 Console.WriteLine($"Введите числа, каждое с новой строки. Для окончания ввода нажмите Enter без ввода числа.");
 int[] array = GetArray();
-Console.WriteLine($"Введено {PosCount(array)} {NumCompletion(PosCount(array))} больше ноля");
+Console.WriteLine($"Введенный массив:\n[{string.Join(", ", array)}]");
+Console.WriteLine();
+Console.WriteLine($"Отсортированный массив:\n[{string.Join(", ", SortArray(array))}]");
+Console.WriteLine();
+
+int[] SortArray(int[] array)
+{
+    int min;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 1; j < array.Length; j++)
+        {
+            if (array[j] < array[i])
+            {
+                Console.WriteLine($"{array[i]}<--->{array[j]}");
+                Console.Write($"[{string.Join(", ", array)}]");
+                Console.Write(" <----> ");
+                min = array[i];
+                array[i] = array[j];
+                array[j] = min;
+                Console.WriteLine($"[{string.Join(", ", array)}]");
+            }
+        }
+    }
+    return array;
+}
+
+// -------------------------Общий метод---------------------------
 
 int[] GetArray()
 {
@@ -15,8 +78,14 @@ int[] GetArray()
     {
         get_curr = Console.ReadLine()!;
         if (get_curr == "") break;
+        if (!int.TryParse(get_curr, out int temp))
+        {
+            Console.WriteLine("Это не число, попробуйте снова:");
+            continue;
+        }
         str = str + "," + get_curr;
     }
+
 
     string[] array = str.Split(new char[] { ',' });
     int[] int_array = new int[array.Length];
@@ -26,32 +95,3 @@ int[] GetArray()
 
     return int_array;
 }
-
-int PosCount(int[] array)
-{
-    int count = 0;
-    foreach (int el in array)    
-        if (el > 0) count++;
-
-    return count;
-}
-
-string NumCompletion(int number)
-{
-    switch (number)
-    {
-        case 1: 
-            return "число";
-        case 2: case 3: case 4:
-            return "числа";
-        default:
-            return "чисел";
-    }
-}
-
-
-//----------------------------------------------------------------
-// Задача 2 (43)
-// Написать программу, которая на вход принимает массив из любого количества элементов 
-// (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, 
-// но отсортированный по возрастанию(от меньшего числа к большему).
