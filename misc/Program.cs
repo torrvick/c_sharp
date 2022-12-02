703c86852e15fd65386e268e58a6dee87ba3be35
Виктор Петкевич <torrvick@gmail.com>
2022-12-02 11:52:16 +0300

    Trying classes

--- misc/Program.cs
+++ misc/Program.cs
@@ -32,3 +32,19 @@ int[] Fib(int num)
     
     return array;
 } */
+
+Person tom = new Person();  // создание объекта класса Person
+tom.name = "Vasya";
+tom.age = 45;
+tom.Print();
+// определение класса Person
+class Person 
+{
+    public string name = "Undefined";
+    public int age;
+ 
+    public void Print()
+    {
+        Console.WriteLine($"Имя: {name}  Возраст: {age}");
+    }
+}
