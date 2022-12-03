28dd4ae67c0f5729e5125afb969fddd4ed7b81a7
Vit <torrvick@gmail.com>
2022-12-03 21:18:58 +0300

    2D arrays

---
+++ misc/fib_dectobin/Program.cs
@@ -0,0 +1,34 @@
+﻿//Десятичное в двоичное
+/* Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+string DecToBin(int dec)
+{
+    string bin = string.Empty;
+    while (dec > 0)
+    {
+        bin = Convert.ToString(dec % 2) + bin;
+        dec /= 2;
+    }
+
+    return bin;
+}
+Console.WriteLine(DecToBin(num)); */
+
+//--------------------------------------------------------------------------
+// Фибоначчи
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"[{String.Join(", ", Fib(num))}]");
+
+int[] Fib(int num)
+{
+    int[] array = new int[num];
+    array[0] = 0;
+    array[1] = 1;
+    for (int i = 2; i < num; i++)
+        array[i] = array[i-1] + array[i-2];
+    
+    return array;
+} 
