3dadaf5e5d08d5231343a651c5584c11fcd60bc0
Виктор Петкевич <torrvick@gmail.com>
2022-12-26 11:10:26 +0300

    fin

---
+++ misc/fin/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] array = {"abc", "defg", "hi", "g", "klmn", "opqrst", "uv", "wxyz"};
+Console.WriteLine();
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[\"{String.Join("\", \"", array)}\"]");
+Console.WriteLine();
+
+string[] ArrayFilter(string[] array)
+{
+    string str_temp = "";
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3) str_temp += array[i] + ",";
+    str_temp = str_temp.Trim(',');
+    string[] array_filtered = str_temp.Split(',');
+    return array_filtered;
+}
+
+Console.WriteLine("Отфильтрованный массив:");
+Console.WriteLine($"[\"{String.Join("\", \"", ArrayFilter(array))}\"]");
+Console.WriteLine();
